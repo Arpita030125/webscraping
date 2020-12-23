f1f3ee8489ff0c021549c6dedb85d258f493c8c4
arpita.bajpai <arpita.bajpai@siemens.com>
2020-12-23 18:57:01 +0530

    2nd commit

--- __mocks__/webScraper.js
+++ __mocks__/webScraper.js
@@ -1,25 +1,26 @@
 const fs = require('fs');
 const cheerio = require('cheerio');
-
+//This file is to test our code in local to run test file using Jest.
 
 module.exports = {
 async processRequest(url)
 {
     console.log("inside web scraper mock function");
    console.log("URl to be processed: ",url);
-   try {
-      //let response = await got(url); 
-    
+   try { 
       let res = await fs.readFileSync("./myPage.html", 'utf8');
-      var resObj = {};
+      var resObj = {}; //Json object returned to user
+      //using cheerio we can navigate through html and get the DOM elements
       let $ = cheerio.load(res);
+      //the first 6 variables get the metadata from HEAD of HTML
       let $title = $('head title').text();
       let $desc = $('meta[name="description"]').attr('content');
       let $kwd = $('meta[name="keywords"]').attr('content');
       let $ogTitle = $('meta[property="og:title"]').attr('content');
       let $ogImage = $('meta[property="og:image"]').attr('content');
       let $ogkeywords = $('meta[property="og:keywords"]').attr('content');
-      let $images = $('img');
+      //gets the data from BODY tag
+      let $images = $('img');  
        if ($title) {
             resObj.title = $title;
         }
--- webScraper.js
+++ webScraper.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const cheerio = require('cheerio');
 const got = require('got'); 
-
+//This file is deployed in AWS lambda function with the actual logic.
 module.exports = {
 async processRequest(url)
 {
@@ -10,14 +10,19 @@ async processRequest(url)
    try {
       let response = await got(url); 
       var resObj = {};
+      //Json object returned to user
+      //using cheerio we can navigate through html and get the DOM elements
       let $ = cheerio.load(response.body);
+       //the first 6 variables get the metadata from HEAD of HTML
       let $title = $('head title').text();
       let $desc = $('meta[name="description"]').attr('content');
       let $kwd = $('meta[name="keywords"]').attr('content');
       let $ogTitle = $('meta[property="og:title"]').attr('content');
       let $ogImage = $('meta[property="og:image"]').attr('content');
       let $ogkeywords = $('meta[property="og:keywords"]').attr('content');
+        //gets the data from BODY tag
       let $images = $('img');
+ //The rest of the lines check whether variables have value  
        if ($title) {
             resObj.title = $title;
         }
